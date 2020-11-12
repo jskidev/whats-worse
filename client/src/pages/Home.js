be5618644dd4a5c16fc28cd4a8cb92ce02cae453
unknown <jeremyskidmoredev@gmail.com>
2020-11-13 00:08:49 +1100

    instructions and responsiveness

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -13,6 +13,8 @@ function Home() {
     const [fadeOut, setFadeOut] = useState(false);
     const [done, setDone] = useState(false);
 
+    const [modalOpen, setModalOpen] = useState(false);
+
     useEffect(() => {
         loadPair()
     }, []);
@@ -91,6 +93,15 @@ function Home() {
         document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
     }
 
+    const handleHelp = () => {
+        if(!modalOpen) {
+            document.body.style.overflow = "hidden";
+        } else {
+            document.body.style.overflowY = "scroll";
+        }
+        setModalOpen(!modalOpen);
+    }
+
     return (
         <>
             <div className="fold">
@@ -122,6 +133,9 @@ function Home() {
                     <a href="#newThing" className="neuLink">
                         <svg height="30" viewBox="0 0 21 21" width="30" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd" stroke="#2a2e3b" stroke-linecap="round" stroke-linejoin="round" transform="translate(6 4)"><path d="m7.328 6.67.001 5.658-5.658-.001" transform="matrix(-.70710678 .70710678 .70710678 .70710678 .965201 -.399799)"/><path d="m4.5.5v13"/></g></svg>
                     </a>
+                    <a className="neuLink help" onClick={handleHelp}>
+                    <svg height="30" viewBox="0 0 21 21" width="30" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd" transform="translate(2 2)"><circle cx="8.5" cy="8.5" r="8" stroke="#2a2e3b" stroke-linecap="round" stroke-linejoin="round"/><path d="m8.5 9.5v-1l1.41421356-1.41421356c.37507274-.37507276.58578644-.88378059.58578644-1.41421356v-.17157288c0-.61286606-.3462631-1.17313156-.89442719-1.4472136l-.21114562-.1055728c-.56305498-.2815275-1.2257994-.2815275-1.78885438 0l-.10557281.0527864c-.61286606.30643303-1 .9328289-1 1.61803399v.88196601" stroke="#2a2e3b" stroke-linecap="round" stroke-linejoin="round"/><circle cx="8.5" cy="12.5" fill="#2a2e3b" r="1"/></g></svg>
+                    </a>
                 </div>
             </div>
             <div className="formContainer">
@@ -136,6 +150,15 @@ function Home() {
                     <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 21 21"><g fill="none" fill-rule="evenodd" stroke="#2A2E3B" stroke-linecap="round" stroke-linejoin="round" transform="translate(6 3)"><polyline points="7.324 1.661 7.324 7.318 1.647 7.339" transform="scale(1 -1) rotate(45 15.35 0)"/><line x1="4.5" x2="4.5" y1=".5" y2="13.5"/></g></svg>
                 </a>
             </div>
+            {modalOpen &&
+                <div className="backdrop">
+                    <div className="neuModal">
+                        <div>Vote on which thing you think is the worst. Add your own things into the mix and let it be voted on.</div>
+                        <button className="neuButtonSecondary" onClick={handleHelp}>next</button>
+                    </div>
+                </div>
+            }
+            
         </>
     )
 }
